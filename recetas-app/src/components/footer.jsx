56ed213d3102c9e583775b8ded0cadf8975b7c9f
ThomasCorrea <tommy16cc@gmail.com>
2025-04-09 14:04:52 -0500

    arreglo footer

--- recetas-app/src/components/footer.jsx
+++ recetas-app/src/components/footer.jsx
@@ -6,64 +6,56 @@ export default function Footer() {
     <div className="footer">
       <div className="horizontal-border">
         <div className="list">
-          <div className="item-link-facebook">
-            <img className="icon" alt="Facebook" src="https://www.svgrepo.com/show/475647/facebook-color.svg" />
-            <div className="facebook">FACEBOOK</div>
+          <div className="item-link">
+            <img className="icon" alt="Facebook" src="https://cdn-icons-png.flaticon.com/512/124/124010.png" />
+            <div className="social-text">FACEBOOK</div>
           </div>
 
-          <div className="item-link-twitter">
-            <img className="img" alt="Twitter" src="https://www.svgrepo.com/show/475689/twitter-color.svg" />
-            <div className="twitter">TWITTER</div>
+          <div className="item-link">
+            <img className="icon" alt="Twitter" src="https://cdn-icons-png.flaticon.com/512/124/124021.png" />
+            <div className="social-text">TWITTER</div>
           </div>
 
-          <div className="item-link-instagram">
-            <img className="icon-2" alt="Instagram" src="https://www.svgrepo.com/show/475702/instagram-color.svg" />
-            <div className="instagram">INSTAGRAM</div>
+          <div className="item-link">
+            <img className="icon" alt="Instagram" src="https://cdn-icons-png.flaticon.com/512/1409/1409946.png" />
+            <div className="social-text">INSTAGRAM</div>
           </div>
 
-          <div className="item-link-pinterest">
-            <img className="icon-2" alt="Pinterest" src="https://www.svgrepo.com/show/475696/pinterest-color.svg" />
-            <div className="pinterest">PINTEREST</div>
+          <div className="item-link">
+            <img className="icon" alt="Pinterest" src="https://cdn-icons-png.flaticon.com/512/174/174863.png" />
+            <div className="social-text">PINTEREST</div>
           </div>
 
-          <div className="item-link-youtube">
-            <img className="img" alt="YouTube" src="https://www.svgrepo.com/show/475703/youtube-color.svg" />
-            <div className="youtube">YOUTUBE</div>
+          <div className="item-link">
+            <img className="icon" alt="YouTube" src="https://cdn-icons-png.flaticon.com/512/1384/1384060.png" />
+            <div className="social-text">YOUTUBE</div>
           </div>
 
-          <div className="item-link-tiktok">
-            <img className="img" alt="TikTok" src="https://www.svgrepo.com/show/475690/tiktok-color.svg" />
-            <div className="tiktok">TIKTOK</div>
+          <div className="item-link">
+            <img className="icon" alt="TikTok" src="https://cdn-icons-png.flaticon.com/512/3046/3046124.png" />
+            <div className="social-text">TIKTOK</div>
           </div>
         </div>
       </div>
 
-      <div className="overlap-group">
-        <div className="text-wrapper">© Divina Cocina 2024</div>
-        <div className="link">
-          <p className="div">– La Ventana Comunicación –</p>
+      <div className="footer-bottom">
+        <div className="info">
+          <div className="text-wrapper">© Divina Cocina 2024</div>
+          <div className="link">
+            <p className="div">La Ventana Comunicación</p>
+          </div>
         </div>
-      </div>
-
-      <div className="div-wrapper">
-        <div className="text-wrapper-2">Aviso Legal</div>
-      </div>
-
-      <div className="link-2">
-        <div className="text-wrapper-3">- Quienes Somos</div>
-      </div>
-
-      <div className="link-3">
-        <div className="text-wrapper-4">- Contacto</div>
-      </div>
-
-      <div className="link-4">
-        <div className="text-wrapper-5">- Sitemap</div>
+        <div className="nav-links">
+          <div className="text-wrapper-2">Aviso Legal</div>
+          <div className="text-wrapper-3">Quienes Somos</div>
+          <div className="text-wrapper-4">Contacto</div>
+          <div className="text-wrapper-5">Sitemap</div>
+        </div>
       </div>
 
       <div className="link-go-to-top">
         <div className="volver-arriba">VOLVER ARRIBA</div>
-        <img className="icon-3" alt="Subir" src="https://www.svgrepo.com/show/521678/arrow-up-bold.svg" />
+        <img className="icon-3" alt="Subir" src="https://cdn-icons-png.flaticon.com/512/892/892692.png" />
       </div>
     </div>
   );
