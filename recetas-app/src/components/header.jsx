258f5cd84249d536d2ebe05f8c521269a151c2c1
ThomasCorrea <tommy16cc@gmail.com>
2025-04-10 17:38:44 -0500

    se agg el header

--- recetas-app/src/components/header.jsx
+++ recetas-app/src/components/header.jsx
@@ -1,7 +1,27 @@
+import React from 'react';
+import '../assets/header.css';
+
 export default function Header() {
   return (
-    <div className="p-5 bg-primary text-white text-center">
-    
-    </div>
-  )
+    <header>
+      <div className="header-top">
+        <h2>Recetas sencillas para alegrarte la vida</h2>
+      </div>
+
+      <nav className="main-nav">
+        <ul className="nav-menu">
+          <li className="active">INICIO</li>
+          <li>CATEGORÍAS <span>v</span></li>
+          <li>RECETAS PARA FREIDORA DE AIRE</li>
+          <li>BLOG <span>v</span></li>
+          <li>VÍDEO RECETAS</li>
+          <li>RECETAS DE TEMPORADA <span>v</span></li>
+        </ul>
+        <div className="search-icon">
+          🔍
+        </div>
+      </nav>
+    </header>
+  );
 }
+
