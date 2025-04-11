22c8ed59b235412f6ceebe0fc256a75f1bdcff33
ThomasCorrea <tommy16cc@gmail.com>
2025-04-11 01:11:33 -0500

    se manda visual y css para cambios de pag

---
+++ recetas-app/src/components/pagination.jsx
@@ -0,0 +1,10 @@
+<div className="pagination-wrapper">
+  <div className="pagination">
+    <button className="page active">1</button>
+    <button className="page">2</button>
+    <button className="page">3</button>
+    <button className="page dots">...</button>
+    <button className="page">304</button>
+    <button className="page">{">"}</button>
+  </div>
+</div>
