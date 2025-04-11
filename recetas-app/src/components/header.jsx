1b1b6f89f8484ac9752d66c3943e674696ee4006
ThomasCorrea <tommy16cc@gmail.com>
2025-04-11 01:25:55 -0500

    se agg el logo y se acomoda el css

--- recetas-app/src/components/header.jsx
+++ recetas-app/src/components/header.jsx
@@ -3,11 +3,16 @@ import '../assets/header.css';
 
 export default function Header() {
   return (
-    <header>
+    <header className="site-header">
       <div className="header-top">
+        <div className="header-logo">
+          <img
+            src="https://www.divinacocina.es/wp-content/uploads/2020/07/divina-cocina.png"
+            alt="Divina Cocina"
+          />
+        </div>
         <h2>Recetas sencillas para alegrarte la vida</h2>
       </div>
     </header>
   );
 }
-
